bf49997e55f0b914e7288c0c32fd6b263d82d0ec
Bojan Strbac <bokasina@gmail.com>
2018-03-22 11:59:29 +0100

    Zadatak 1 resen, koeficijenti

--- processing.c
+++ processing.c
@@ -4,11 +4,11 @@
 void calculateShelvingCoeff(float c_alpha, Int16* output)
 {
 	Int16 k1, k2;
-	k1 = c_alpha * 32768;
+	k1 = c_alpha * 32767;
 	k2  = -32768*(c_alpha);
 
-	if(output[0] > 32768) {
-		output[0] = 32768;
+	if(output[0] > 32767) {
+		output[0] = 32767;
 	}
 	else {
 		output[0] = k1;
@@ -29,11 +29,12 @@ void calculateShelvingCoeff(float c_alpha, Int16* output)
 void calculatePeekCoeff(float c_alpha, float c_beta, Int16* output)
 {
 
+
 	output[0] = c_alpha * 32768;
-	output[1] =	(-c_beta * (32768 + c_alpha))/2;
-	output[2] = 32768;
-	output[3] = 32768;
-	output[4] = (-c_beta * (32768 + c_alpha))/2;
+	output[1] =	(-c_beta * (1 + c_alpha))*16384;
+	output[2] = 32767;
+	output[3] = 32767;
+	output[4] = (-c_beta * (1 + c_alpha))*16384;
 	output[5] = c_alpha * 32768;
 
 
@@ -42,6 +43,7 @@ void calculatePeekCoeff(float c_alpha, float c_beta, Int16* output)
 
 Int16 shelvingHP(Int16 input, Int16* coeff, Int16* x_history, Int16* y_history, Int16 k)
 {
+
 	/* Your code here */
 	return 0;
 }
